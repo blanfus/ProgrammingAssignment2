60b86c2655342aaee29e1eddf6a1c3daf6c59953
blanfus <bfuster26@gmail.com>
2015-01-23 20:20:51 +0100

    Third version. More comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,42 @@
 ## Calculate the inverse of a matrix, keeping into account if it has been calculated yet.
+## For this assignment, we can assume that the matrix supplied is always invertible (as stated
+## in the assigment, so catching exceptions are not necessary
 
 
+## makeCacheMatrix <- function(x = matrix())
 ## Generate a list to keep together a matrix and its associated inverse matrix, with 
 ## function elements to get data kept in cache (yet calculated) and set data for the first time
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
+    env <- environment()
+    parent_env <- parent.env(env)
     set <- function(y) {
         ## initialize matrix
         x <<- y
-        ## initialize inverse
+        ## initialize inverse matrix. It has not been calculated yet
         inv <<- NULL
     }
     ## gets the matrix
     get <- function() x
     ## calculates the inverse
     setinverse <- function(solve) inv <<- solve
-    ## get the inverse matrix yet calculated
+    ## gets the inverse matrix yet calculated
     getinverse <- function() inv
-    
+    ## some stuff for educative purpose only
+    getenv <- function() env
+    getpar_env<-function() parent_env
     ## result
     list(set = set, get = get,
           setinverse = setinverse,
-          getinverse = getinverse)
+          getinverse = getinverse,
+          getenv=getenv,
+          getpar_env=getpar_env)
     
 }
 
 
+## cacheSolve <- function(x, ...)
 ## Lets calculate the inverse of a matrix (defined as a list with the function makeCacheMatrix)
 ## If the inverse has been yet calculated, it shows the result. If not, calculates it and
 ## this inverse matrix gets included in the list, together with the origina matrix
